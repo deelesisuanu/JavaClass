0881fc6a57915e912be8d10a4998fc8099184658
Deelesi Suanu <deelesisuanu@gmail.com>
2020-09-05 16:30:34 +0100

    chore(add-operator-empty): Added Empty Operator Files

--- Examples/Hello World/HelloWorld.java
+++ Examples/Hello World/HelloWorld.java
@@ -1,10 +1,12 @@
 public class HelloWorld {
 
     // This is a main method that runs our code.
-    public static void main(String[] args) {
+    public static void main(String[] rich) {
+        System.out.println(rich);
         System.out.println("Welcome to Java Learning Class"); // This Outputs Welcome to Java Learning Class
         String name = "";
         System.out.println("Hello World Java !!!");
+        Comment();
     }
 
     // Single Comment
@@ -13,5 +15,18 @@ public class HelloWorld {
      * Multiple Line Comment
      * 
      */
+
+    static void Comment(){
+        System.out.println("I am Commenting");
+
+        Comment("Please Comment", 5);
+    }
+    static void Comment(String word, int letter, int numOfTimes){}
+     
+    static void Comment(int letter, String word){}
+
+    static void Comment(String word, int numOfTimes){
+        System.out.println(word + " " + numOfTimes + " times");
+    }
 
 }
